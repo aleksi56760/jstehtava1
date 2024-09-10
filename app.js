08eb26adf933731211e683c057b0f1a0687c2760
Aleksi <aleksi.kahkonen@edu.salpaus.fi>
2024-09-10 10:58:27 +0300

    age box js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("javascript")
+function agefunk() {
+  let age = document.getElementById("ikä").value;
+  let ikäinen = (age < 18) ? "Alaikäinen":"Täysi-ikäinen";
+  document.getElementById("paragraph").innerHTML = ikäinen;
+}
+console.log("Ran JS Script")
